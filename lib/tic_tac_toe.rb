ba7e6bad10760234fbd630f97092c823ddf28494
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-17 23:09:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,3 +55,107 @@ def valid_move?(board, i)
     return false
   end
 end
+
+# encapulates the logic of a single complete turn
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  isvalid = valid_move?(board, index)
+
+  if isvalid == true
+    move(board, index)
+    display_board(board)
+  else
+    puts "Sorry, invalid move :("
+    turn(board)
+  end
+end
+
+# returns the number of turns that have been played
+def turn_count(board)
+  counter = 0
+  board.each do |char|
+    if char == 'X' || char == 'O'
+      counter += 1
+    end
+  end
+  counter
+end
+
+# determines who is the current player (X or O)
+def current_player(board)
+  turn_count(board) % 2 == 1 ? 'O' : 'X'
+end
+
+# returns the winner combination or returns false if there is no winner combination
+def won?(board)
+
+  WIN_COMBINATIONS.each do |win_arr|
+
+    yes_x = win_arr.all? do |i|
+      board[i] == 'X'
+    end
+
+    if yes_x == true
+      return win_arr
+    end
+
+    yes_o = win_arr.all? do |i|
+      board[i] == 'O'
+    end
+
+    if yes_o == true
+      return win_arr
+    end
+  end
+
+  return nil
+
+end
+
+# returns true if the whole board is full
+def full?(board)
+  board.none? {|el| el == "" || el == " "}
+end
+
+
+# returns true if the board is full but the game has not been won, returns false if the game has not been won and the board is not full or if the game is won
+def draw?(board)
+
+  if full?(board) == true
+    if won?(board) == nil
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
+
+end
+
+# returns true if the board has been won, is draw or is full
+def over?(board)
+
+  if won?(board) != nil || draw?(board) == true || full?(board) == true
+    return true
+  else
+    return false
+  end
+
+end
+
+# returns the winner token
+def winner(board)
+
+  if won?(board) != nil
+    winner_arr_firstindex = won?(board)[0]
+    if board[winner_arr_firstindex] == "X"
+      return "X"
+    elsif board[winner_arr_firstindex] == "O"
+      return "O"
+    end
+  end
+
+end
